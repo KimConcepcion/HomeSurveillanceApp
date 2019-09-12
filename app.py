2beb15bb40c3bb2090ccaa1946ffec6dc253a440
root <root@beaglebone.localdomain>
2019-09-12 19:49:33 +0000

    tested out cpu temp + fans

--- app.py
+++ app.py
@@ -4,25 +4,24 @@ import psutil as ps
 app = Flask(__name__)
 
 ## Network stuff:
-#HOST = '192.168.1.34'
-HOST = 'localhost'
+HOST = '192.168.1.34'
+#HOST = 'localhost'
 PORT = '5000'
 
 ## CPU stuff:
 cpu_threads = ps.cpu_count(logical=True)
 cpu_cores = ps.cpu_count(logical=False)
-cpu_prc = ps.cpu_percent(interval=0.5, percpu=False)
+cpu_prc = ps.cpu_percent(interval=1, percpu=True)
 cpu_frq = ps.cpu_freq(percpu=True)
-cpu_temp = ps.sensors_temperatures(fahrenheit=False)
+#cpu_temp = ps.sensors_temperatures(fahrenheit=False)
 
 ## Index Route:
 @app.route('/')
 def index():
     return render_template(
         'index.html',
-        threads=cpu_threads, cores=cpu_cores, percent=cpu_prc, frequency=cpu_frq[0], temperature=cpu_temp
+        threads=cpu_threads, cores=cpu_cores, percent=cpu_prc, frequency=cpu_frq
         )
-    
 ## Error Route:
 @app.errorhandler(404)
 def not_found(e):
