8ba4b08ff44bf6acc19ea23615e90afea39b827e
root <root@beaglebone.localdomain>
2019-09-22 21:05:56 +0000

    Fixed favicon

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ import psutil as ps
 app = Flask(__name__)
 
 ## Network stuff:
-#HOST = '192.168.1.34'
-HOST = 'localhost'
+HOST = '192.168.1.34'
+#HOST = 'localhost'
 PORT = '5000'
 
 ## Index Route:
 @app.route('/')
 def index():
-    
+
     ## CPU Stuff - Update per request:
     cpu_threads = ps.cpu_count(logical=True)
     cpu_cores = ps.cpu_count(logical=False)
     cpu_avg_prc = ps.cpu_percent(1, percpu=False)
     cpu_frq = ps.cpu_freq(percpu=True)
     current_frq = cpu_frq[0][0]
-    
+
     return render_template(
         'index.html', threads=cpu_threads, cores=cpu_cores, current_speed=current_frq, cpu_average_load=cpu_avg_prc
         )
-    
+
 ## Error Route:
 @app.errorhandler(404)
 def not_found(e):
@@ -30,4 +30,4 @@ def not_found(e):
 
 ## Start server:
 if __name__ == '__main__':
-    app.run(host=HOST, port=PORT, debug=True)
+    app.run(host=HOST, port=PORT, debug=False)
